0ea7983d6d65b416a7249bd198a72ece2452b8ca
santiago <167025641+fabzt@users.noreply.github.com>
2025-05-23 22:20:01 -0300

    Add files via upload

---
+++ BlocoDeNotas.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace blocoDeNotasSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.BackColor = System.Drawing.Color.Khaki;
+            this.txtConteudo.Location = new System.Drawing.Point(232, 83);
+            this.txtConteudo.Multiline = true;
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConteudo.Size = new System.Drawing.Size(300, 200);
+            this.txtConteudo.TabIndex = 0;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAbrir.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAbrir.Location = new System.Drawing.Point(232, 303);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnSalvar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSalvar.Location = new System.Drawing.Point(343, 303);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnLimpar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnLimpar.Location = new System.Drawing.Point(457, 303);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(309, 58);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(145, 22);
+            this.lblTitulo.TabIndex = 4;
+            this.lblTitulo.Text = "Editor de texto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtConteudo);
+            this.Name = "Form1";
+            this.Text = "Abrir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ BlocoDeNotas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace blocoDeNotasSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Arquivo de Texto (*.txt)|*.txt";
+            abrir.Title = "Abrir arquivo";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                txtConteudo.Text = File.ReadAllText(abrir.FileName);
+                MessageBox.Show("Arquivo carregado com sucesso!", "Abrir", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog salvar = new SaveFileDialog();
+            salvar.Filter = "Arquivo de Texto (*.txt)|*.txt";
+            salvar.Title = "Salvar como";
+
+            if (salvar.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(salvar.FileName, txtConteudo.Text);
+                MessageBox.Show("Arquivo salvo com sucesso!", "Salvar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtConteudo.Clear();
+        }
+    }
+}
